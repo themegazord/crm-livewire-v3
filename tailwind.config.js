f611ce45cd75246f94fa754561b16f81ff0e3987
Gustavo de Camargo Campos <contato.wanjalagus@outlook.com.br>
2024-10-22 18:27:16 -0400

    instalando maryui

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,12 +5,13 @@ import typo from "@tailwindcss/typography";
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
-    "./storage/framework/views/*.php",
-    "./resources/**/*.blade.php",
-    "./resources/**/*.js",
-    "./resources/**/*.vue",
-  ],
+		"./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
+		 "./storage/framework/views/*.php",
+		 "./resources/**/*.blade.php",
+		 "./resources/**/*.js",
+		 "./resources/**/*.vue",
+		 "./vendor/robsontenorio/mary/src/View/Components/**/*.php"
+	],
   theme: {
     extend: {
       fontFamily: {
@@ -18,5 +19,7 @@ export default {
       },
     },
   },
-  plugins: [],
+  plugins: [
+		require("daisyui")
+	],
 };
