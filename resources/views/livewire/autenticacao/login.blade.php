c22af9bf7cad97b6e1a46f88a6d627f6c0b39e50
Gustavo de Camargo Campos <contato.wanjalagus@outlook.com.br>
2024-10-23 15:07:57 -0400

    adicionado alert do maryui para ratelimiter message

--- resources/views/livewire/autenticacao/login.blade.php
+++ resources/views/livewire/autenticacao/login.blade.php
@@ -1,14 +1,18 @@
-<x-card shadow class="mx-auto w-[32rem]">
-  <x-form wire:submit="submit">
-    <x-input label="Insira seu email" wire:model="email"/>
+<x-card shadow class="mx-auto w-[32rem]" title="Login">
+  @if ($errors->hasAny(['rateLimiter']))
+  <x-alert icon="o-exclamation-triangle" class="alert-warning mb-4">
     @error('rateLimiter')
-      <span class="text-red-500 label-text-alt">{{ $message }}</span>
+    <span>{{ $message }}</span>
     @enderror
-    <x-input label="Insira sua senha" wire:model="password" type="password" password-icon="o-lock-closed" password-visible-icon="o-lock-open"  clearable/>
+  </x-alert>
+  @endif
+  <x-form wire:submit="submit">
+    <x-input label="Insira seu email" wire:model="email" />
+    <x-input label="Insira sua senha" wire:model="password" type="password" password-icon="o-lock-closed" password-visible-icon="o-lock-open" clearable />
 
 
     <x-slot:actions>
-      <x-button label="Limpar campos" type="reset"/>
+      <x-button label="Limpar campos" type="reset" />
       <x-button label="Logar" class="btn-primary" type="submit" spinner="save" />
     </x-slot:actions>
   </x-form>
