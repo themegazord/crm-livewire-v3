7e2cf40ff59a3453f1ac2ed67a734dec2ac53a7e
Gustavo de Camargo Campos <contato.wanjalagus@outlook.com.br>
2024-10-23 15:01:47 -0400

    configurado dashboard para ser rota autenticada

--- routes/web.php
+++ routes/web.php
@@ -6,21 +6,13 @@ use App\Livewire\Welcome;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
-Route::get('/', Welcome::class)->name('dashboard');
 Route::get('/registro', Registro::class)->name('autenticacao.registro');
-Route::get('/login', Login::class)->name('autenticacao.login');
+Route::get('/login', Login::class)->name('login');
 Route::get('/logout', function () {
   Auth::logout();
   return redirect('/');
+});
+
+Route::middleware('auth')->group(function () {
+  Route::get('/', Welcome::class)->name('dashboard');
 });
