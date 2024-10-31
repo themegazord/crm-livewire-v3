e2c58cb1f2c7b829c2d7b95e451cef9fe49c1472
Gustavo de Camargo Campos <contato.wanjalagus@outlook.com.br>
2024-10-31 16:19:46 -0400

    wip

--- tests/Feature/Admin/GerenciamentoUsuario/DetalharUsuariosTest.php
+++ tests/Feature/Admin/GerenciamentoUsuario/DetalharUsuariosTest.php
@@ -16,9 +16,9 @@ it('ser capaz de mostrar todos os dados do cliente num form somente leitura', fu
     ->set('usuario', $usuario)
     ->assertSee($usuario->name)
     ->assertSee($usuario->email)
-    ->assertSee($usuario->created_at)
-    ->assertSee($usuario->updated_at);
-});
+    ->assertSee(date('d/m/Y H:i:s', strtotime($usuario->created_at)))
+    ->assertSee(date('d/m/Y H:i:s', strtotime($usuario->updated_at)));
+  });
 
 test('se o usuario contem remover deve mostrar o nome de quem removeu o cadastro e o deleted_at', function () {
   /** @var User $admin */
@@ -29,46 +29,46 @@ test('se o usuario contem remover deve mostrar o nome de quem removeu o cadastro
   actingAs($admin);
 
   Livewire::test(Usuarios\Remover::class)
-    ->set('confirmacao_confirmation', $usuario->name)
+  ->set('confirmacao_confirmation', $usuario->name)
     ->call('configuraModalDeConfirmacao', $usuario->id)
     ->call('destroy');
 
-  $usuario->refresh();
+    $usuario->refresh();
 
-  if (expect($usuario->remover_id === $admin->id) && !is_null($usuario->deleted_at)) {
-    Livewire::test(Usuarios\Detalhar::class)
+    if (expect($usuario->remover_id === $admin->id) && !is_null($usuario->deleted_at)) {
+      Livewire::test(Usuarios\Detalhar::class)
       ->set('usuario', $usuario)
       ->assertSee($usuario->remover->name)
-      ->assertSee($usuario->deleted_at);
-  }
-});
+      ->assertSee(date('d/m/Y H:i:s', strtotime($usuario->deleted_at)));
+    }
+  });
 
-test('se o usuario contem restorer_id deve mostrar o nome de quem restaurou o cadastro e o restored_at', function () {
-  /** @var User $admin */
-  $admin = User::factory()->admin()->create();
-  /** @var User $usuario */
-  $usuario = User::factory()->create();
+  test('se o usuario contem restorer_id deve mostrar o nome de quem restaurou o cadastro e o restored_at', function () {
+    /** @var User $admin */
+    $admin = User::factory()->admin()->create();
+    /** @var User $usuario */
+    $usuario = User::factory()->create();
 
-  actingAs($admin);
+    actingAs($admin);
 
-  Livewire::test(Usuarios\Remover::class)
+    Livewire::test(Usuarios\Remover::class)
     ->set('confirmacao_confirmation', $usuario->name)
     ->call('configuraModalDeConfirmacao', $usuario->id)
     ->call('destroy');
 
-  $usuario->refresh();
+    $usuario->refresh();
 
-  Livewire::test(Usuarios\Restaurar::class)
+    Livewire::test(Usuarios\Restaurar::class)
     ->set('confirmacao_confirmation', $usuario->name)
     ->call('configuraModalDeConfirmacao', $usuario->id)
     ->call('restore');
 
-  $usuario->refresh();
+    $usuario->refresh();
 
-  if (expect($usuario->restored_id === $admin->id) && !is_null($usuario->restored_at)) {
+    if (expect($usuario->restored_id === $admin->id) && !is_null($usuario->restored_at)) {
     Livewire::test(Usuarios\Detalhar::class)
-      ->set('usuario', $usuario)
-      ->assertSee($usuario->restorer->name)
-      ->assertSee($usuario->restored_at);
+    ->set('usuario', $usuario)
+    ->assertSee($usuario->restorer->name)
+    ->assertSee(date('d/m/Y H:i:s', strtotime($usuario->restored_at)));
   }
 });
